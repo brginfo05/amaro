8b461980ed2d043f77e47ac295006e67dc818d3e
bruno <brginfo05@gmail.com>
2019-09-29 18:07:22 -0300

    fixing swagger

--- src/main/java/br/com/test/amaro/config/SwaggerConfig.java
+++ src/main/java/br/com/test/amaro/config/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
   public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2)
         .select()
-        .apis(RequestHandlerSelectors.basePackage("br.com.teste.amaro"))
+        .apis(RequestHandlerSelectors.basePackage("br.com.test.amaro"))
         .paths(PathSelectors.any())
         .build();
   }
